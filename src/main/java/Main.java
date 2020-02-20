b3835785c577c7cd6d29abbb84c648bc926ce4d9
WhoKnocks <6548515+WhoKnocks@users.noreply.github.com>
2020-02-20 22:00:28 +0100

    score per dag jenge

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -33,6 +33,9 @@ public class Main {
         System.out.println("Reading e");
         run("e_so_many_books.txt");
 
+        System.out.println("Reading f");
+        run("f_libraries_of_the_world.txt");
+
         System.out.println("barts mom");
         System.out.println("is a milf!");
     }
